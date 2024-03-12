12f9ab3129b0cdffcbc9f9abe37109708e2683dc
JoyceVanelle <tsatiamomo@gmail.com>
2024-03-11 21:45:21 -0400

    connexion
    nouvelle

--- app/Http/Controllers/ClientsController.php
+++ app/Http/Controllers/ClientsController.php
@@ -93,10 +93,22 @@ class ClientsController extends Controller
             return response()->json(['error' => 'Could not create token'], 500);
         }
 
+        $user = Auth::user();
+
+        $userData = [
+            'id' => $user->id,
+            'name' => $user->name,
+            'prenom' => $user->prenom,
+            'email' => $user->email
+        ];
+
         // If successful, return the JWT token with other relevant user data
         return response()->json([
-            'token' => $token,
-            'user' => Auth::user() 
+          
+            'message'=>"test",
+           // 'user' => Auth::user() 
+           'user' => $userData
+
         ]);
     }
 }
